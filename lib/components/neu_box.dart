233a1520295f537694a76ebef2c8c624363dacff
Naman Mahajan <mahajannaman2020@gmail.com>
2025-04-19 17:22:16 +0530

    song page

---
+++ lib/components/neu_box.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class NeuBox extends StatelessWidget {
+  final Widget? ChilD;
+  const NeuBox({super.key, required this.ChilD});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).colorScheme.primary,
+            blurRadius: 15,
+            offset: const Offset(4, 4),
+          ),
+          BoxShadow(
+            color: Theme.of(context).colorScheme.surface,
+            blurRadius: 15,
+            offset: const Offset(-4, -4),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(12),
+      child: ChilD,
+    );
+  }
+}
--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/components/neu_box.dart';
+import 'package:music_player/models/playlist_provider.dart';
+import 'package:provider/provider.dart';
 
 class SongPage extends StatelessWidget {
   const SongPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Consumer<PlaylistProvider>(
+        builder:
+            (context, value, child) => Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              body: Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  right: 25,
+                  bottom: 25,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.arrow_back),
+                        ),
+                        Text("P L A Y L I S T"),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                      ],
+                    ),
+                    NeuBox(
+                      ChilD: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.asset("lib/assets/images/pic1.jpg"),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "so sick",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Text("Neyo"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+      ),
+    );
   }
 }
